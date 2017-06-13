1d29d8448015df88264142c4c65bc6a711ec2e72
sunjieming <ben@lambdaschool.com>
2017-06-12 18:02:27 -0700

    Converted from ES6 -> ES5

--- callbacks.js
+++ callbacks.js
@@ -9,7 +9,7 @@
  *
  *  You would then define the greeting function to make the invocation work.
  *
- *  const greeting = (str, cb) => {
+ *  var greeting = (str, cb) => {
  *    cb(str);
  *  };
  *
@@ -18,34 +18,34 @@
 
 // Write a function called firstItem that passes the first item of the given array to the callback function
 
-const foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
+var foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
 
 firstItem(foods, (firstItem) => {
-  console.log(`The first item is ${firstItem}.`);
+  console.log('The first item is ' + firstItem);
 });
 
 // Write a function called getLength that passes the length of the array into the callback
 
 getLength(foods, (length) => {
-  console.log(`The length of the array is ${length}.`);
+  console.log('The length of the array is ' + length);
 });
 
 // Write a function called last which passes the last item of the array into the callback
 
 last(foods, (lastItem) => {
-  console.log(`The last item in the array is ${lastItem}.`);
+  console.log('The last item in the array is ' + lastItem);
 });
 
 // Write a function called sumNums that adds two numbers and passes the result to the callback
 
 sumNums(5, 10, (sum) => {
-  console.log(`The sum is ${sum}.`);
+  console.log('The sum is ' + sum);
 });
 
 // Write a function called multiplyNums that adds two numbers and passes the result to the callback
 
 multiplyNums(5, 10, (product) => {
-  console.log(`The product is ${product}.`);
+  console.log('The product is ' + product);
 });
 
 // Write a function called contains that checks if an item is present inside of the given array.
@@ -59,11 +59,11 @@ contains(foods, 'ribeye', (result) => {
 // Pass the array to the callback function.  Do not mutate the original array.
 
 removeDuplicates(foods, (uniqueFoods) => {
-  console.log(`foods with duplicates removed: ${uniqueFoods}`);
+  console.log('foods with duplicates removed: ' + uniqueFoods);
 });
 
 // Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
 
 forEach(foods, (value, index) => {
-  console.log(`${value} is at index ${index}.`);
+  console.log('${value} is at index ' + index);
 });
