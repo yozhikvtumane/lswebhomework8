df319d8d5560c7f05aaa25a7f337a28a6335c0e1
sunjieming <ben@lambdaschool.com>
2017-06-15 12:20:15 -0700

    Refactor from ES6 -> ES5

--- callbacks.js
+++ callbacks.js
@@ -20,50 +20,50 @@
 
 var foods = ['pineapple', 'mango', 'ribeye', 'curry', 'tacos', 'ribeye', 'mango'];
 
-firstItem(foods, (firstItem) => {
+firstItem(foods, function(firstItem) {
   console.log('The first item is ' + firstItem);
 });
 
 // Write a function called getLength that passes the length of the array into the callback
 
-getLength(foods, (length) => {
+getLength(foods, function(length) {
   console.log('The length of the array is ' + length);
 });
 
 // Write a function called last which passes the last item of the array into the callback
 
-last(foods, (lastItem) => {
+last(foods, function(lastItem) {
   console.log('The last item in the array is ' + lastItem);
 });
 
 // Write a function called sumNums that adds two numbers and passes the result to the callback
 
-sumNums(5, 10, (sum) => {
+sumNums(5, 10, function(sum) {
   console.log('The sum is ' + sum);
 });
 
 // Write a function called multiplyNums that adds two numbers and passes the result to the callback
 
-multiplyNums(5, 10, (product) => {
+multiplyNums(5, 10, function(product) {
   console.log('The product is ' + product);
 });
 
 // Write a function called contains that checks if an item is present inside of the given array.
 // Pass true to the callback if it is, otherwise pass false
 
-contains(foods, 'ribeye', (result) => {
+contains(foods, 'ribeye', function(result) {
   console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
 });
 
 // Write a function called removeDuplicates that removes all duplicate values from the given array.
 // Pass the array to the callback function.  Do not mutate the original array.
 
-removeDuplicates(foods, (uniqueFoods) => {
+removeDuplicates(foods, function(uniqueFoods) {
   console.log('foods with duplicates removed: ' + uniqueFoods);
 });
 
 // Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
 
-forEach(foods, (value, index) => {
-  console.log('${value} is at index ' + index);
+forEach(foods, function(value, index) {
+  console.log(value + ' is at index ' + index);
 });
